52e2671f1952f2486880df74b0d9693f4419a8d2
Enutro <noah.durtellede@gmail.com>
2024-07-25 16:15:07 +0200

    Ajout fonctionnalité

--- app/routes/edit.py
+++ app/routes/edit.py
@@ -35,6 +35,20 @@ class CFAUpdate(BaseModel):
     complement: Optional[str]
     code_postal: Optional[str]
 
+class EmployeurUpdate(BaseModel):
+    nom_entreprise: Optional[str]
+    numero_siret: Optional[str]
+    numero: Optional[str]
+    rue: Optional[str]
+    complement: Optional[str]
+    code_postal: Optional[str]
+    commune: Optional[str]
+    code_IDCC: Optional[str]
+    effectif: Optional[str]
+    code_activite_NAF: Optional[str]
+    courriel: Optional[str]
+    telephone: Optional[str]
+
 @router.get("/users")
 async def get_Users(current_user: dict = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
     result = await db.execute(
@@ -55,6 +69,30 @@ async def get_Users(current_user: dict = Depends(get_current_user), db: AsyncSes
             "prenom": row[1],
             "email": row[2],
             "id": row[3]
+            })
+
+    return users
+
+@router.get("/employeurs")
+async def get_Employeurs(current_user: dict = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+    select(
+        Employeur.id,
+        Employeur.courriel,
+        Employeur.nom_entreprise,
+        Employeur.telephone,
+    )
+    )
+
+    all_data = result.all()
+    
+    users = []
+    for row in all_data:
+        users.append({
+            "id": row[0],
+            "courriel": row[1],
+            "nom_entreprise": row[2],
+            "telephone": row[3]
             })
 
     return users
@@ -82,6 +120,47 @@ async def get_user(id: int, current_user: dict = Depends(get_current_user), db: 
         "email": user_data.email,
         "nom_entreprise": user_data.nom_entreprise,
         "nom_role": user_data.nom_role,
+    }
+
+    return response_data
+
+router.get("/get/employeur/{id}")
+async def get_employeur(id: int, current_user: dict = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(
+            Employeur.nom_entreprise,
+            Employeur.numero_siret,
+            Employeur.numero,
+            Employeur.rue,
+            Employeur.complement, 
+            Employeur.code_postal,
+            Employeur.commune,
+            Employeur.code_IDCC,
+            Employeur.effectif,
+            Employeur.code_activite_NAF,
+            Employeur.courriel,
+            Employeur.telephone
+        ).where(Employeur.id == id)
+    )
+
+    user_data = result.first()
+    
+    if user_data is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    response_data = {
+        "nom_entreprise": user_data.nom_entreprise,
+        "numero_siret": user_data.numero_siret,
+        "numero": user_data.numero,
+        "rue": user_data.rue,
+        "complement": user_data.complement,
+        "code_postal": user_data.code_postal,
+        "commune": user_data.commune,
+        "code_IDCC": user_data.code_IDCC,
+        "effectif": user_data.effectif, 
+        "code_activite_NAF": user_data.code_activite_NAF,
+        "courriel": user_data.courriel,
+        "telephone": user_data.telephone
     }
 
     return response_data
@@ -152,6 +231,16 @@ async def delete_user(id: int, db: AsyncSession = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     await db.delete(user)
+    await db.commit()
+    return {"message": "User deleted successfully"}
+
+@router.delete("/delete/employeur/{id}")
+async def delete_employeur(id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Employeur).filter(Employeur.id == id))
+    employeur = result.scalars().first()
+    if not employeur:
+        raise HTTPException(status_code=404, detail="User not found")
+    await db.delete(employeur)
     await db.commit()
     return {"message": "User deleted successfully"}
 
